f937dece3eff10c5307d9298596319a7be7fbf49
Matt Ho <methodho@gmail.com>
2014-12-16 13:03:42 +0800

    Update app and add Matt service for server

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,74 +7,11 @@
  * # MainCtrl
  * Controller of the HarvardApp
  */
-HarvardApp.controller('MainCtrl', ['$scope', '$timeout', '$log', '$modal', 'ganttUtils', 'GanttObjectModel', 'Coloured', 'Harvard', 'ganttMouseOffset', 'ganttDebounce', 'moment', function($scope, $timeout, $log, $modal, utils, ObjectModel, Coloured, Harvard, mouseOffset, debounce, moment) {
+HarvardApp.controller('MainCtrl', ['$scope', '$timeout', '$log', '$modal', 'ganttUtils', 'GanttObjectModel', 'Coloured', 'Harvard', 'Matt', 'ganttMouseOffset', 'ganttDebounce', 'moment', function($scope, $timeout, $log, $modal, utils, ObjectModel, Coloured, Harvard, Matt, mouseOffset, debounce, moment) {
     var objectModel;
     var dataToRemove;
 
-    $scope.sortableOptions = {
-        accept: function (sourceItemHandleScope, destSortableScope) {
-          return sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id;
-        }
-    };
-    $scope.itemsList = {
-        items: []
-    };
-    for (var i = 0; i <= 5; i += 1) {
-        $scope.itemsList.items.push({'Id': i, 'Label': 'Item ' + i});
-    }
-
-
-    $scope.configuration = {
-        serverLocation: '',
-        jsLocationPrefix: '/',
-        saveGanttUrl: '/calculate',
-        calcGanttUrl: '/calculate',
-        // 以字串(term)模糊查詢符合的PoNo
-        // url: GET /company/sales/pos/like/{term}
-        // return: list of labelValueModel, label: poNo, value: poNo
-        poFuzzySearch: '/company/sales/pos/like/#poNo#',
-
-        // 以PoNo取得Po資料
-        // url: GET /company/sales/po/{poNo}
-        // return: po data
-        getPoUrl: '/company/sales/po/#poNo#',
-
-        // 以PoId取得其下的Combo
-        // url: GET /company/sales/combo/by-po/{PoId}
-        // return: list of labelValueModel, label: comboId, value: comboId
-        getComboUrl: '/company/sales/combo/by-po/#poId#',
-
-        // 以ComboId取得其下的Product
-        // uurl: GET /company/sales/combo/product/by-combo/{comboId}
-        // return list of labelValeModel, label: product info, value: productId
-        getProductUrl: '/company/sales/combo/product/by-combo/#comboId#',
-
-        // 取poNo, comboId, productId資訊
-        getProductInfoUrl: '/aps/operations/pdt-info/#taskId#',
-
-        // 以ProductId取得其下的Process
-        // url: GET /aps/processes/by-product/{productId}?factoryOperationCode={factoryOperationCode}
-        // request params:
-        // factoryOperationCode, default: blank, 有給的話會過濾出所屬該factoryOperationCode的process, 沒給的話不過濾
-        // return list of labelValueModel, label: process info, value processId
-        // ?factoryOperationCode=#factoryOperationCode#
-        getProcessUrl: '/aps/processes/by-product/#productId#',
-
-        // url: /aps/operations/{operationIds} (operationIds用逗號分隔)
-        getMoreInformationPage: '/aps/operations/#operationIds#',
-
-        // confirm
-        // url: POST or PUT /company/scheduler/plans/confirm
-        // request params:
-
-        // save, defautl: false, 是否要儲存
-        // check, default: true, 計算前是否要執行precondition check?
-        // calculate, defaul: true, 是否要計算
-        // calculateFrom, default: system day + 1 day, 從哪一天開始計算
-        // calculateWeeks, default: 12, 要計算幾周
-        // return tw.com.softleader.harvard.aps.service.result.ApsMessage
-        confirmGanttUrl: '/company/scheduler/plans/calculate/',
-    };
+    $scope.configuration = Matt.configuration();
 
     $scope.defaultScale = ['minute', '5 minutes', 'hour', '3 hours', '6 hours', '8 hours', 'day', 'week', '2 weeks', 'month', 'quarter', '6 months', 'year'];
 
