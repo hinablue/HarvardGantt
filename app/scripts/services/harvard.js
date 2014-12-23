8d1218e6779b9337594a9032373340da968d65c9
Hina Chen <hinablue@gmail.com>
2014-12-23 14:40:07 +0800

    Remove useless UI

--- app/scripts/services/harvard.js
+++ app/scripts/services/harvard.js
@@ -12597,62 +12597,7 @@ angular.module('HarvardApp')
   ],
   'messages': []
 };
-                // return [
-                //         {name: 'Status meetings', tasks: [
-                //             {name: 'Demo #1', color: '#9FC5F8', from: new Date(2013, 9, 25, 15, 0, 0), to: new Date(2013, 9, 25, 18, 30, 0)},
-                //             {name: 'Demo #2', color: '#9FC5F8', from: new Date(2013, 10, 1, 15, 0, 0), to: new Date(2013, 10, 1, 18, 0, 0)},
-                //             {name: 'Demo #3', color: '#9FC5F8', from: new Date(2013, 10, 8, 15, 0, 0), to: new Date(2013, 10, 8, 18, 0, 0)},
-                //             {name: 'Demo #4', color: '#9FC5F8', from: new Date(2013, 10, 15, 15, 0, 0), to: new Date(2013, 10, 15, 18, 0, 0)},
-                //             {name: 'Demo #5', color: '#9FC5F8', from: new Date(2013, 10, 24, 9, 0, 0), to: new Date(2013, 10, 24, 10, 0, 0)}
-                //         ]},
-                //         {name: 'Kickoff', movable: {allowResizing: false}, tasks: [
-                //             {name: 'Day 1', color: '#9FC5F8', from: new Date(2013, 9, 7, 9, 0, 0), to: new Date(2013, 9, 7, 17, 0, 0),
-                //                 progress: {percent: 100, color: '#3C8CF8'}, movable: false},
-                //             {name: 'Day 2', color: '#9FC5F8', from: new Date(2013, 9, 8, 9, 0, 0), to: new Date(2013, 9, 8, 17, 0, 0),
-                //                 progress: {percent: 100, color: '#3C8CF8'}},
-                //             {name: 'Day 3', color: '#9FC5F8', from: new Date(2013, 9, 9, 8, 30, 0), to: new Date(2013, 9, 9, 12, 0, 0),
-                //                 progress: {percent: 100, color: '#3C8CF8'}}
-                //         ]},
-                //         {name: 'Create concept', tasks: [
-                //             {name: 'Create concept', color: '#F1C232', from: new Date(2013, 9, 10, 8, 0, 0), to: new Date(2013, 9, 16, 18, 0, 0), est: new Date(2013, 9, 8, 8, 0, 0), lct: new Date(2013, 9, 18, 20, 0, 0),
-                //                 progress: 100}
-                //         ]},
-                //         {name: 'Finalize concept', tasks: [
-                //             {name: 'Finalize concept', color: '#F1C232', from: new Date(2013, 9, 17, 8, 0, 0), to: new Date(2013, 9, 18, 18, 0, 0),
-                //                 progress: 100}
-                //         ]},
-                //         {name: 'Sprint 1', tooltips: false, tasks: [
-                //             {name: 'Product list view', color: '#F1C232', from: new Date(2013, 9, 21, 8, 0, 0), to: new Date(2013, 9, 25, 15, 0, 0),
-                //                 progress: 25}
-                //         ]},
-                //         {name: 'Sprint 2', tasks: [
-                //             {name: 'Order basket', color: '#F1C232', from: new Date(2013, 9, 28, 8, 0, 0), to: new Date(2013, 10, 1, 15, 0, 0)}
-                //         ]},
-                //         {name: 'Sprint 3', tasks: [
-                //             {name: 'Checkout', color: '#F1C232', from: new Date(2013, 10, 4, 8, 0, 0), to: new Date(2013, 10, 8, 15, 0, 0)}
-                //         ]},
-                //         {name: 'Sprint 4', tasks: [
-                //             {name: 'Login & Signup & Admin Views', color: '#F1C232', from: new Date(2013, 10, 11, 8, 0, 0), to: new Date(2013, 10, 15, 15, 0, 0)}
-                //         ]},
-                //         {name: 'Setup server', tasks: [
-                //             {name: 'HW', color: '#F1C232', from: new Date(2013, 10, 18, 8, 0, 0), to: new Date(2013, 10, 18, 12, 0, 0)}
-                //         ]},
-                //         {name: 'Config server', tasks: [
-                //             {name: 'SW / DNS/ Backups', color: '#F1C232', from: new Date(2013, 10, 18, 12, 0, 0), to: new Date(2013, 10, 21, 18, 0, 0)}
-                //         ]},
-                //         {name: 'Deployment', tasks: [
-                //             {name: 'Depl. & Final testing', color: '#F1C232', from: new Date(2013, 10, 21, 8, 0, 0), to: new Date(2013, 10, 22, 12, 0, 0), 'classes': 'gantt-task-deployment'}
-                //         ]},
-                //         {name: 'Workshop', tasks: [
-                //             {name: 'On-side education', color: '#F1C232', from: new Date(2013, 10, 24, 9, 0, 0), to: new Date(2013, 10, 25, 15, 0, 0)}
-                //         ]},
-                //         {name: 'Content', tasks: [
-                //             {name: 'Supervise content creation', color: '#F1C232', from: new Date(2013, 10, 26, 9, 0, 0), to: new Date(2013, 10, 29, 16, 0, 0)}
-                //         ]},
-                //         {name: 'Documentation', tasks: [
-                //             {name: 'Technical/User documentation', color: '#F1C232', from: new Date(2014, 10, 26, 8, 0, 0), to: new Date(2014, 10, 28, 18, 0, 0)}
-                //         ]}
-                //     ];
+
             }
         };
     })
